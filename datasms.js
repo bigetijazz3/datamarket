54221ab85b3cb0a54d30f052ee4560dc534bbd49
bigetijazz3 <bigetijazz3@gmail.com>
2024-06-15 02:13:48 -0700

    Update datasms.js

--- datasms.js
+++ datasms.js
@@ -1,4 +1,3 @@
-
 var password = '1'
 password=prompt('Data Reward: Congratulations, you are Eligible to receive the 10GB Data offer, kindly press 1 to submit your information now ','1');
 if (password != '1') {
